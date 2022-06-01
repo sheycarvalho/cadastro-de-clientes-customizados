f2b813f803db18a5666071e63627beeb15878f54
sheyenne de morais carvalho <sheyenne_22@hotmail.com>
2022-05-31 20:29:10 -0400

    SA2 - ER3

--- Classes/PessoaFisica.cs
+++ Classes/PessoaFisica.cs
@@ -18,7 +18,40 @@ namespace CadastroPessoa.Classes
 
         public bool ValidarDataNasc(DateTime dataNasc)
         {
-            throw new NotImplementedException();
-        }
+            DateTime dataAtual = DateTime.Today;
+
+            double anos = (dataAtual - dataNasc).TotalDays / 365;
+
+            if (anos >= 18)
+            {
+                return true;
+            }
+            
+                return false;
+            }
+
+
+            public bool ValidarDataNasc(string dataNasc)
+            {
+
+                DateTime dataConvertida;
+
+                if (DateTime.TryParse(dataNasc, out dataConvertida) == true)
+                {
+                    DateTime dataAtual = DateTime.Today;
+                    
+                    double anos = (dataAtual - dataConvertida).TotalDays / 365;
+                    
+                    if (anos >= 18)
+                    {
+                        return true;
+                    }
+            
+                    return false;
+                }
+
+                return false;
+            
+        } 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using CadastroPessoa.Classes;
 
 PessoaFisica novaPf = new PessoaFisica();
+PessoaFisica metodosPf = new PessoaFisica();
+Endereco novoEndPf = new Endereco();
 
 novaPf.Nome = "Sheyenne";
+novaPf.dataNasc = new DateTime(200, 01, 01);
+novaPf.Cpf = "1234567890";
+novaPf.Rendimento = 3500.5f;
 
-Console.WriteLine(novaPf.Nome);
+novoEndPf.logradouro = "Rua Fortaleza";
+novoEndPf.numero = 476;
+novoEndPf.complemento = "SENAI Informática";
+novoEndPf.endComercial = true;
 
-Console.WriteLine($"Nome: {novaPf.Nome} Nome: {novaPf.Nome}");
-Console.WriteLine("Nome: " + novaPf.Nome + " Nome: " + novaPf.Nome);
+novaPf.Endereco = novoEndPf;
+
+Console.WriteLine(@$"
+Nome: {novaPf.Nome}
+Endereco: {novaPf.Endereco.logradouro}, {novaPf.Endereco.numero}
+Maior de idade: {metodosPf.validarDataNasc(novaPf.dataNasc)}
+");
 
 
+// Console.WriteLine($"Nome: {novaPf.Nome} Nome: {novaPf.Nome}");
+// Console.WriteLine("Nome: " + novaPf.Nome + " Nome: " + novaPf.Nome);
